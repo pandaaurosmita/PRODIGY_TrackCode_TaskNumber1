0c382086acd202a5340602d0a48f8212f14f6b1e
Aurosmita Panda <79589437+pandaaurosmita@users.noreply.github.com>
2024-06-22 11:05:08 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,16 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.getElementById('navbar');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
+
+
+
+
